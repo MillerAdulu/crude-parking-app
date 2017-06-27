9739ed2fc5854bffd5779e9faf8329f3ee9ce12d
MillerAdulu <milleradulu@gmail.com>
2017-06-27 23:02:56 +0300

    Added input sanitization.

--- dynamic_data/ValidateLogin.php
+++ dynamic_data/ValidateLogin.php
@@ -1,11 +1,12 @@
 <?php
     require_once "../classes/SessionManagement.php";
+    $user = $GLOBALS["user"];
+    $login = new SessionManagement;
 
     if(isset($_POST) && isset($_POST["username"]) && isset($_POST["password"])){
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+        $user->sanitizeInput($username = $_POST["username"]);
+        $user->sanitizeInput($password = $_POST["password"]);
 
-            $login = new SessionManagement;
             $login->login($username, $password);
 
         }
